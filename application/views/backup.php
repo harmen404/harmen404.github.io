945c9a8fa02d760b4510153f7e032f158aa76f26
harmen404 <harmenisra@gmail.com>
2023-04-28 17:06:38 +0700

    first commit

---
+++ application/views/backup.php
@@ -0,0 +1,365 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Portofolio</title>
+
+    <link rel="stylesheet" href="<?php echo base_url('assets/');?>css/bootstrap.min.css">
+    <link rel="stylesheet" href="<?php echo base_url('assets/');?>css/unicons.css">
+    <link rel="stylesheet" href="<?php echo base_url('assets/');?>css/owl.carousel.min.css">
+    <link rel="stylesheet" href="<?php echo base_url('assets/');?>css/owl.theme.default.min.css">
+
+    <!-- MAIN STYLE -->
+    <link rel="stylesheet" href="<?php echo base_url('assets/');?>css/tooplate-style.css">
+    
+<!--
+
+Tooplate 2115 Marvel
+
+https://www.tooplate.com/view/2115-marvel
+
+-->
+  </head>
+  <body>
+
+    <!-- MENU -->
+    <nav class="navbar navbar-expand-sm navbar-light">
+        <div class="container">
+            <a class="navbar-brand" href="index.php"><i class='uil uil-user'></i> Harmen Isra</a>
+
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false"
+                aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+                <span class="navbar-toggler-icon"></span>
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav mx-auto">
+                    <li class="nav-item">
+                        <a href="#about" class="nav-link"><span data-hover="About">About</span></a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="#project" class="nav-link"><span data-hover="Projects">Projects</span></a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="#resume" class="nav-link"><span data-hover="Resume">Resume</span></a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="#contact" class="nav-link"><span data-hover="Contact">Contact</span></a>
+                    </li>
+                </ul>
+
+                <ul class="navbar-nav ml-lg-auto">
+                    <div class="ml-lg-4">
+                      <div class="color-mode d-lg-flex justify-content-center align-items-center">
+                        <i class="color-mode-icon"></i>
+                        Color mode
+                      </div>
+                    </div>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+    <!-- ABOUT -->
+    <section class="about full-screen d-lg-flex justify-content-center align-items-center" id="about">
+        <div class="container">
+            <div class="row">
+                
+                <div class="col-lg-7 col-md-12 col-12 d-flex align-items-center">
+                    <div class="about-text">
+                        <small class="small-text">Welcome to <span class="mobile-block">my portfolio website!</span></small>
+                        <h1 class="animated animated-text">
+                            <span class="mr-2">Hey , I'm</span>
+                                <div class="animated-info">
+                                    <span class="animated-item">interest on</span>
+                                    <span class="animated-item">Backend Dev</span>
+                                    <span class="animated-item">Database MS</span>
+                                </div>
+                        </h1>
+
+                        <p>Bachelor of Informatics Engineering. Looking for job opportunities in the IT field but it doesn’t rule out the possibility
+for me started a position in another field, successfully graduated on time with a GPA of 3,81. I'm a person who
+always wants to develop themselves in learning new things to meet challenging work experiences and
+unforgettable. Enjoy working both in a team and individually. Have the ability good time management,
+communication and interpersonal.</p>
+                        
+                        <div class="custom-btn-group mt-4">
+                        <a href="https://drive.google.com/file/d/18Bj2zylD4KCah_urdMc5ryiP0oL1gSrb/view?usp=sharing" class="btn mr-lg-2 custom-btn"><i class='uil uil-file-alt'></i> My Curriculum Vitae (indonesia)</a>
+                          <a href="https://drive.google.com/file/d/1WtEbXGP2VB3WejpmQ6sloKA-DWCbyex0/view?usp=sharing" class="btn custom-btn custom-btn-bg custom-btn-link">My Curriculum Vitae (English)</a> 
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-lg-5 col-md-12 col-12">
+                    <div class="about-image svg">
+                        <img src="<?php echo base_url('assets/');?>images/undraw/undraw_software_engineer_lvl5.svg" class="img-fluid" alt="svg image">
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </section>
+
+    <!-- PROJECTS -->
+    <section class="project py-5" id="project">
+        <div class="container">
+                
+                <div class="row">
+                  <div class="col-lg-11 text-center mx-auto col-12">
+
+                      <div class="col-lg-8 mx-auto">
+                        <h2>My Little project is</h2>
+                      </div>
+
+                      <div class="owl-carousel owl-theme">
+                        <div class="item">
+                          <div class="project-info">
+                            <!-- <img src="<?php echo base_url('assets/');?>images/project/project-image01.png" class="img-fluid" alt="project image">
+                          </div>
+                        </div>
+
+                        <div class="item">
+                          <div class="project-info">
+                            <img src="<?php echo base_url('assets/');?>images/project/project-image02.png" class="img-fluid" alt="project image">
+                          </div>
+                        </div> -->
+
+                        <div class="item">
+                          <div class="project-info">
+                            <img src="<?php echo base_url('assets/');?>images/project/project-image03.png" class="img-fluid" alt="project image">
+                          </div>
+                        </div>
+
+                        <!-- <div class="item">
+                          <div class="project-info">
+                            <img src="<?php echo base_url('assets/');?>images/project/project-image04.png" class="img-fluid" alt="project image">
+                          </div>
+                        </div>
+
+                        <div class="item">
+                          <div class="project-info">
+                            <img src="<?php echo base_url('assets/');?>images/project/project-image05.png" class="img-fluid" alt="project image">
+                          </div>
+                        </div> -->
+                      </div>
+
+                  </div>
+                </div>
+        </div>
+    </section>
+
+    <!-- FEATURES -->
+    <section class="resume py-5 d-lg-flex justify-content-center align-items-center" id="resume">
+        <div class="container">
+            <div class="row">
+
+                <div class="col-lg-6 col-12">
+                  <h2 class="mb-4">Experiences</h2>
+
+                    <div class="timeline">
+                        <div class="timeline-wrapper">
+                             <div class="timeline-yr">
+                                  <span>2020</span>
+                             </div>
+                             <div class="timeline-info">
+                                  <h3><span>Internship Back office Kantor Desa Tualang</span><small>Regional office</small></h3>
+                                  <p>● Input data SIKS NG, BLT, & KIS</p>
+                                  <p>● Recapitulate SLRT data</p>
+                                  <p>● Mail Archive</p>
+                             </div>
+                        </div>
+
+                        <div class="timeline-wrapper">
+                            <div class="timeline-yr">
+                                <span>2015</span>
+                            </div>
+                            <div class="timeline-info">
+                                <h3><span>Internship PT. Indah Kiat Pulp & Paper </span><small>Paper Industri</small></h3>
+                                <p>● CA (Chlor Alkali) (Lubrication Centrifugal Pump, Change Mechanical Seal Centrifugal Pump, Fabrikasi Pipa Reed Caustic)</p>
+                                <p>● EN (Change Conveyor Belt, Lubrication Gearbox, Repair Water Cooling Tower)</p>
+                                <p>● WT (Water Environment) (Replacing Centrifugal Pump Impeller, Lubrication Centrifugal Pump And Vertical Pump, Replacing Oil Seal)</p>
+                            </div>
+                        </div>
+<!-- 
+                        <div class="timeline-wrapper">
+                            <div class="timeline-yr">
+                                <span>2016</span>
+                            </div>
+                            <div class="timeline-info">
+                                <h3><span>UI Freelancer</h3>
+                                <p>Sed fringilla vitae enim sit amet cursus. Sed cursus dictum tortor quis pharetra. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.</p>
+                            </div>
+                        </div>
+                        
+                        <div class="timeline-wrapper">
+                            <div class="timeline-yr">
+                                <span>2014</span>
+                            </div>
+                            <div class="timeline-info">
+                                <h3><span>Junior Designer<small>Crafted Co.</small></h3>
+                                <p>Cras scelerisque scelerisque condimentum. Nullam at volutpat mi. Nunc auctor ipsum eget magna consequat viverra.</p>
+                            </div>
+                        </div>
+ -->
+                    </div>
+                </div>
+
+                <div class="col-lg-6 col-12">
+                  <h2 class="mb-4 mobile-mt-2">Educations</h2>
+
+                    <div class="timeline">
+                        <div class="timeline-wrapper">
+                             <div class="timeline-yr">
+                                  <span>2021</span>
+                             </div>
+                             <div class="timeline-info">
+                                  <h3><span>Universitas Abdurrab Pekanbaru</span><small></small></h3>
+                                  <h4><span>Teknik Informatika</h4></span>
+                                  <p>Graduated 3.81 of 4.0 GPA</p>
+                             </div>
+                        </div>
+
+                        <div class="timeline-wrapper">
+                            <div class="timeline-yr">
+                                <span>2017</span>
+                            </div>
+                            <div class="timeline-info">
+                                <h3><span>SMK YPPI Tualang, Perawan</span><small></small></h3>
+                                <h4><span>Teknik Mesin</h4></span>
+                                <P>Graduated 81 of 100 GPA </P>
+                                <!-- <p><a rel="nofollow" href="https://www.facebook.com/tooplate">Tooplate</a> is a great website to download HTML templates without any login or email.</p> -->
+                            </div>
+                        </div>
+                        
+                        <!-- <div class="timeline-wrapper">
+                            <div class="timeline-yr">
+                                <span>2013</span>
+                            </div>
+                            <div class="timeline-info">
+                                <h3><span>Artwork Design</span><small>New Art School</small></h3>
+                                <p>You can freely use Tooplate's templates for your business or personal sites. You cannot redistribute this template without a permission.</p>
+                            </div>
+                        </div> -->
+
+                        <div class="col-lg-6 col-12">
+                  <h2 class="mb-4 mobile-mt-2">Skill</h2>
+
+                    <div class="timeline">
+                        <div class="timeline-wrapper">
+                             <div class="timeline-yr">
+                                  <span>2021</span>
+                             </div>
+                             <div class="timeline-info">
+                                  <h3><span>Universitas Abdurrab Pekanbaru</span><small></small></h3>
+                                  <h4><span>Teknik Informatika</h4></span>
+                                  <p>Graduated 3.81 of 4.0 GPA</p>
+                             </div>
+                        </div>
+
+                        <div class="timeline-wrapper">
+                            <div class="timeline-yr">
+                                <span>2017</span>
+                            </div>
+                            <div class="timeline-info">
+                                <h3><span>SMK YPPI Tualang, Perawan</span><small></small></h3>
+                                <h4><span>Teknik Mesin</h4></span>
+                                <P>Graduated 81 of 100 GPA </P>
+                                <!-- <p><a rel="nofollow" href="https://www.facebook.com/tooplate">Tooplate</a> is a great website to download HTML templates without any login or email.</p> -->
+                            </div>
+                        </div>
+                
+
+                    </div>
+                </div>
+                
+            </div>
+        </div>
+    </section>
+
+    <!-- CONTACT -->
+    <section class="contact py-5" id="contact">
+      <div class="container">
+        <div class="row">
+          
+          <div class="col-lg-5 mr-lg-5 col-12">
+            <div class="google-map w-100">
+              
+              <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1994.7717145809909!2d101.59889692021427!3d0.6725800286480775!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x31d44ea4b38f11ad%3A0xbf645ecd4d429fb3!2sJl.%20Indah%20Kasih%2C%20Perawang%2C%20Kec.%20Tualang%2C%20Kabupaten%20Siak%2C%20Riau%2028685!5e0!3m2!1sid!2sid!4v1666759332735!5m2!1sid!2sid" width="400" height="300" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+            </div>
+
+            <div class="contact-info d-flex justify-content-between align-items-center py-4 px-lg-5">
+                <div class="contact-info-item">
+                  <h3 class="mb-3 text-white">Hello</h3>
+                  <p class="footer-text mb-0">Contact me on Email</p>
+                  <p><a href="mailto:hello@company.co">harmenisra@gmail.com</a></p>
+                </div>
+
+                <ul class="social-links">
+                     <li><a href="https://www.linkedin.com/in/harmenisra/" class="uil uil-dribbble" data-toggle="tooltip" data-placement="left" title="LinkedIn"></a></li>
+                     <!-- <li><a href="#" class="uil uil-instagram" data-toggle="tooltip" data-placement="left" title="Instagram"></a></li>
+                     <li><a href="#" class="uil uil-youtube" data-toggle="tooltip" data-placement="left" title="linkedin"></a></li> -->
+                </ul>
+            </div>
+          </div>
+
+          <div class="col-lg-6 col-12">
+            <div class="contact-form">
+              <h2 class="mb-4">I'm still a beginner if you Interested to work together? Let's talk</h2>
+
+              <form action="" method="get">
+                <div class="row">
+                  <div class="col-lg-6 col-12">
+                    <input type="text" class="form-control" name="name" placeholder="Your Name" id="name">
+                  </div>
+
+                  <div class="col-lg-6 col-12">
+                    <input type="email" class="form-control" name="email" placeholder="Email" id="email">
+                  </div>
+
+                  <div class="col-12">
+                    <textarea name="message" rows="6" class="form-control" id="message" placeholder="Message"></textarea>
+                  </div>
+
+                  <div class="ml-lg-auto col-lg-5 col-12">
+                    <input type="submit" class="form-control submit-btn" value="Send Button">
+                  </div>
+                </div>
+              </form>
+            </div>
+          </div>
+
+        </div>
+      </div>
+    </section>
+
+    <!-- FOOTER -->
+     <footer class="footer py-5">
+          <div class="container">
+               <div class="row">
+
+                    <div class="col-lg-12 col-12">                                
+                        <p class="copyright-text text-center">Copyright &copy; 2019 Company Name . All rights reserved</p>
+                        <p class="copyright-text text-center">Designed by <a rel="nofollow" href="https://www.facebook.com/tooplate">Tooplate</a></p>
+                    </div>
+                    
+               </div>
+          </div>
+     </footer>
+
+    <script src="<?php echo base_url('assets/');?>js/jquery-3.3.1.min.js"></script>
+    <script src="<?php echo base_url('assets/');?>js/popper.min.js"></script>
+    <script src="<?php echo base_url('assets/');?>js/bootstrap.min.js"></script>
+    <script src="<?php echo base_url('assets/');?>js/Headroom.js"></script>
+    <script src="<?php echo base_url('assets/');?>js/jQuery.headroom.js"></script>
+    <script src="<?php echo base_url('assets/');?>js/owl.carousel.min.js"></script>
+    <script src="<?php echo base_url('assets/');?>js/smoothscroll.js"></script>
+    <script src="<?php echo base_url('assets/');?>js/custom.js"></script>
+
+  </body>
+</html>
